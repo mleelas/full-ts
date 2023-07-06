64494e995faac89bdfd8973f251329702ef1ce57
mleelas <mlsankar@outlook.com>
2023-07-06 23:52:04 +0530

    more types

--- scr/annotations/variables.ts
+++ scr/annotations/variables.ts
@@ -10,3 +10,25 @@ let nothing: undefined = undefined;
 
 // build in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//classes
+class Car {}
+
+let car: Car = new Car();
+
+//Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+//Function
+const logNumber = (i: number) => {
+  console.log(i);
+};

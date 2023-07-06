ba134dd9061e4f65ff5b5666467995cf0f18b0fc
mleelas <mlsankar@outlook.com>
2023-07-06 23:13:59 +0530

    types

--- scr/annotations/variables.ts
+++ scr/annotations/variables.ts
@@ -1,1 +1,12 @@
-const apples: number = 5;
+let apples: number = 5;
+
+apples = 10;
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// build in objects
+let now: Date = new Date();
